4cdb1f04562fc4a45ae31147dc5efe010f580d8c
ShahzadSarwar10 <e@cognitiveconvergence.com>
2025-03-18 17:36:22 +0500

    changes in int type

---
+++ Week1/Case5-SpecificDataType.py
@@ -0,0 +1,26 @@
+# Specific Data Type
+strVar = str("Hello World - Richard")
+print(type(strVar))
+
+strVar2="Hello World - Richard"
+print(type(strVar2))
+
+
+#---------------------- int
+
+intVar = int(50)
+print(type(intVar))
+
+intVar2=50
+print(type(intVar2))
+
+
+#---------------------- float
+
+floatVar = float(50.3)
+print(type(floatVar))
+
+floatVar2=50
+print(type(floatVar2))
+
+#---------------------
