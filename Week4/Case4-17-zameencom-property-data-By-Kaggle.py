55ebb01d90b17d152290c199485b80a2e42a4193
ShahzadSarwar10 <e@cognitiveconvergence.com>
2025-04-17 16:51:03 +0500

    Refined - Dataframe - Pandas examples  -  code with Real estate data
    Refined - Dataframe - Pandas examples  -  code with Real estate data

--- Week4/Case4-17-zameencom-property-data-By-Kaggle.py
+++ Week4/Case4-17-zameencom-property-data-By-Kaggle.py
@@ -89,38 +89,56 @@ Used for Reference   Row and column labels (names)                    Numerical 
 """
 #Selecting a single row using .loc
 second_row = df.loc[1]
+print("#Selecting a single row using .loc")
 print(second_row)
+print()
 
 #Selecting multiple rows using .loc
 second_row2 = df.loc[[1, 3]]
+print("#Selecting multiple rows using .loc")
 print(second_row2)
+print()
 
 #Selecting a slice of rows using .loc
 second_row3 = df.loc[1:5]
+print("#Selecting a slice of rows using .loc")
 print(second_row3)
+print()
+
 
 #Conditional selection of rows using .loc
 second_row4 = df.loc[df['agency'] == 'Gateway Properties']
+print("#Conditional selection of rows using .loc")
 print(second_row4)
+print()
 
 #Selecting a single column using .loc
 second_row5 = df.loc[:1,'agency']
+print("#Selecting a single column using .loc")
 print(second_row5)
+print()
 
 #Selecting multiple columns using .loc
 second_row6 = df.loc[:1,['agency','agent']]
+print("#Selecting multiple columns using .loc")
 print(second_row6)
+print()
 
 #Selecting a slice of columns using .loc
 second_row7 = df.loc[:1,'location':'agency']
+print("#Selecting a slice of columns using .loc")
 print(second_row7)
+print()
 
 #Combined row and column selection using .loc
 second_row8 = df.loc[df['agency'] == 'Gateway Properties','location':'agency']
+print("#Combined row and column selection using .loc")
 print(second_row8)
+print()
 # Case 1 : using .loc - default case - ends here
 
 
+print("# Case 2 : using .loc with index_col - starts here")
 # Case 2 : using .loc with index_col - starts here
 # Second cycle - with index_col as property_id
 # Why Second cycle - Note Index - , index_col='property_id'
@@ -133,39 +151,57 @@ print(df_index_col.info())
 
 #Selecting a single row using .loc
 second_row = df_index_col.loc[482892]
+print("#Selecting a single row using .loc")
 print(second_row)
+print()
 
 #Selecting multiple rows using .loc
 second_row2 = df_index_col.loc[[482892, 555962]]
+print("#Selecting multiple rows using .loc")
 print(second_row2)
+print()
 
 #Selecting a slice of rows using .loc
 second_row3 = df_index_col.loc[482892:686990]
+print("#Selecting a slice of rows using .loc")
 print(second_row3)
+print()
 
 #Conditional selection of rows using .loc
 second_row4 = df_index_col.loc[df_index_col['agency'] == 'Gateway Properties']
+print("#Conditional selection of rows using .loc")
 print(second_row4)
+print()
 
 #Selecting a single column using .loc
 second_row5 = df_index_col.loc[:686990,'agency']
+print("#Selecting a single column using .loc")
 print(second_row5)
+print()
+
 
 #Selecting multiple columns using .loc
 second_row6 = df_index_col.loc[:686990,['agency','agent']]
+print("#Selecting multiple columns using .loc")
 print(second_row6)
+print()
 
 #Selecting a slice of columns using .loc
 second_row7 = df_index_col.loc[:686990,'location':'agency']
+print("#Selecting a slice of columns using .loc")
 print(second_row7)
+print()
 
 #Combined row and column selection using .loc
 second_row8 = df_index_col.loc[df_index_col['agency'] == 'Gateway Properties','location':'agency']
+print("#Combined row and column selection using .loc")
 print(second_row8)
+print()
 
 # Case 2 : using .loc with index_col  -  ends here
 
 
+print("# Case 3 : Using .iloc - starts here")
 # Case 3 : Using .iloc - starts here
 """Using .iloc: Selection by Integer Position
 .iloc selects by position instead of label. This is the standard syntax of using .iloc: df.iloc[row_indexer, column_indexer]. There are two special things to look out for:
@@ -175,31 +211,45 @@ Exclusivity of range end value: When using a slice, the row or column specified 
 
 #Selecting a single row using .iloc
 second_row = df_index_col.iloc[0]
+print("#Selecting a single row using .iloc")
 print(second_row)
+print()
 
 #Selecting multiple rows using .iloc
 second_row2 = df_index_col.iloc[[1, 3,5]]
+print("#Selecting multiple rows using .iloc")
 print(second_row2)
+print()
 
 #Selecting a slice of rows using .iloc
 second_row3 = df_index_col.iloc[2:5]
+print("#Selecting a slice of rows using .iloc")
 print(second_row3)
+print()
 
 #Selecting a single column using .iloc
 second_row5 = df_index_col.iloc[:,2]
+print("#Selecting a single column using .iloc")
 print(second_row5)
+print()
 
 #Selecting multiple columns using .iloc
 second_row6 = df_index_col.iloc[:,[2,4]]
+print("#Selecting multiple columns using .iloc")
 print(second_row6)
+print()
 
 #Selecting a slice of columns using .iloc
 second_row7 = df_index_col.iloc[:,2:4]
+print("#Selecting a slice of columns using .iloc")
 print(second_row7)
+print()
 
 #Combined row and column selection using .iloc
 second_row8 = df_index_col.iloc[[1, 3,5],2:4]
+print("#Combined row and column selection using .iloc")
 print(second_row8)
+print()
 
 # Case 3 : Using .iloc - ends here
 
@@ -221,6 +271,7 @@ Renaming rows/columns"""
 df.loc[len(df.index)] = [3477952,82,"https://www.zameen.com/Property/lahore_model_town_6_kanal_excellent_house_for_sale_in_model_town-347795-8-12.html","House2",2200000002,"Model Town2","Lahore2","Punjab2",312.483868658082,742.325685501099,2,"6 Kanal2","For Sale2",2,"07-17-2019","Real Biz International2","Usama Khan2"] 
 print("Modified DataFrame - add a new row:")
 print(df)
+print()
 
 
 #Remove Rows/Columns from a Pandas DataFrame
@@ -268,6 +319,101 @@ df.rename(mapper={1: 10, 2: 100}, axis=0, inplace=True)
 print("Modified DataFrame - Rename Row - 0  >>> 7 , 1 >>> 10 , 2 >>> 100  Labels:")
 print(df)
 
+
+
+#query() to Select Data
+#The query() method in Pandas allows you to select data using a more SQL-like syntax.
+
+# select the rows where the age is greater than 25
+selected_rows = df.query('agency == \'Gateway Properties\' or price > 11000000')
+
+print(selected_rows.to_string())
+print(len(selected_rows))
+
+
+
+# sort DataFrame by price in ascending order
+sorted_df = df.sort_values(by='price')
+print(sorted_df.to_string(index=False))
+
+#Sort Pandas DataFrame by Multiple Columns
+
+# 1. Sort DataFrame by 'Age' and then by 'Score' (Both in ascending order)
+df1 = df.sort_values(by=['price', 'location_id'])
+
+print("Sorting by 'price' (ascending) and then by 'location_id' (ascending):\n")
+print(df1.to_string(index=False))
+
+
+"""
+Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-to_string/
+Explanation: This code creates a DataFrame from a dictionary with three columns (Weight, Name, Age), structures it into a tabular format using pd.DataFrame() and converts it into a fully visible string representation with df.to_string().
+
+Syntax
+DataFrame.to_string(buf=None, columns=None, col_space=None, header=True, index=True, na_rep=’NaN’, formatters=None, float_format=None, sparsify=None, index_names=True, justify=None, max_rows=None, max_cols=None, show_dimensions=False, decimal=’.’, line_width=None)
+
+
+Parameters:
+
+buf: Buffer to write the output string to (e.g., a file). Defaults to None, which means the output is returned as a string.
+columns: Specifies a subset of columns to include in the output. If None, all columns are printed.
+col_space: Defines the minimum width of each column.
+header: Whether to print column names. Can also accept a list of column name aliases.
+index: Whether to include index labels. Default is True.
+na_rep: String representation for missing values (NaN). Default is ‘NaN’.
+formatters: Dictionary or list of functions to apply to columns for formatting their output.
+float_format: Formatter function to apply specifically to floating-point numbers.
+sparsify: Controls hierarchical index formatting. If False, prints every multi-index key at each row.
+index_names: Whether to print index names. Default is True.
+justify: Alignment of column headers (‘left’, ‘right’, ‘center’, ‘justify’ or ‘justify-all’).
+max_rows: Maximum number of rows to display. If exceeded, truncates output.
+max_cols: Maximum number of columns to display. If exceeded, truncates output.
+show_dimensions: If True, displays the shape (rows x columns) of the DataFrame.
+decimal: Specifies the character for decimal separation (e.g., ‘,’ for European formatting).
+line_width: Defines the maximum character width of a row before wrapping text."""
+
+
+
+#Pandas groupby
+#In Pandas, the groupby operation lets us group data based on specific columns. This means we can divide a DataFrame into smaller groups based on the values in these columns.
+
+# group the DataFrame by the location_id column and
+# calculate the sum of price for each category
+grouped = df.groupby('location_id')['price'].sum()
+
+print(grouped.to_string())
+print("grouped :" , len(grouped))
+
+
+""""Pandas Data Cleaning
+Data cleaning means fixing and organizing messy data. Pandas offers a wide range of tools and functions to help us clean and preprocess our data effectively.
+"""
+# use dropna() to remove rows with any missing values
+df_cleaned = df.dropna()
+print("Cleaned Data:\n",df_cleaned)
+
+
+# filling NaN values with 0
+df.fillna(0, inplace=True)
+
+print("\nData after filling NaN with 0:\n", df)
+
+
+
+# create a list named data
+data = [2, 4, 6, 8]
+# create Pandas array using data
+array1 = pd.array(data)
+print(array1)
+"""<IntegerArray>
+[2, 4, 6, 8]
+Length: 4, dtype: Int64"""
+
+
+# creating a pandas.array of integers
+int_array = pd.array([1, 2, 3, 4, 5], dtype='int')
+print(int_array)
+print()
 
 #Pandas Reshape
 #In Pandas, reshaping data refers to the process of converting a DataFrame from one format to another for better data visualization and analysis.
