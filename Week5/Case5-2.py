b6ee46d875d77272d9d84d24f4513a60a2f24205
ShahzadSarwar10 <e@cognitiveconvergence.com>
2025-04-25 16:26:36 +0500

    Week 5 - Sci Kit Learn Sample  Code
    Week 5 - Sci Kit Learn Sample  Code

--- Week5/Case5-2.py
+++ Week5/Case5-2.py
@@ -33,6 +33,14 @@ names = [
     "QDA",
 ]
 
+""""
+https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
+
+Classifier comparison
+A comparison of several classifiers in scikit-learn on synthetic datasets. The point of this example is to illustrate the nature of decision boundaries of different classifiers. This should be taken with a grain of salt, as the intuition conveyed by these examples does not necessarily carry over to real datasets.
+
+Particularly in high-dimensional spaces, data can more easily be separated linearly and the simplicity of classifiers such as naive Bayes and linear SVMs might lead to better generalization than is achieved by other classifiers."""
+
 classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025, random_state=42),
@@ -48,12 +56,43 @@ classifiers = [
     QuadraticDiscriminantAnalysis(),
 ]
 
+#https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_classification.html
+"""
+sklearn.datasets.make_classification(n_samples=100, n_features=20, *, n_informative=2, n_redundant=2, n_repeated=0, n_classes=2, n_clusters_per_class=2, weights=None, flip_y=0.01, class_sep=1.0, hypercube=True, shift=0.0, scale=1.0, shuffle=True, random_state=None)[source]
+
+Generate a random n-class classification problem.
+
+This initially creates clusters of points normally distributed (std=1) about vertices of an n_informative-dimensional hypercube with sides of length 2*class_sep and assigns an equal number of clusters to each class. It introduces interdependence between these features and adds various types of further noise to the data."""
 X, y = make_classification(
     n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=1
 )
+
+print("dataset - X:  " , X)
+print("dataset - y:  " , y)
+
+
 rng = np.random.RandomState(2)
 X += 2 * rng.uniform(size=X.shape)
 linearly_separable = (X, y)
+
+print("linearly_separable: " , linearly_separable)
+
+"""make_moons
+
+https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_moons.html
+
+sklearn.datasets.make_moons(n_samples=100, *, shuffle=True, noise=None, random_state=None)[source]
+Make two interleaving half circles."""
+
+
+""""make_circles
+
+https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html
+
+sklearn.datasets.make_circles(n_samples=100, *, shuffle=True, noise=None, random_state=None, factor=0.8)[source]
+Make a large circle containing a smaller circle in 2d.
+
+A simple toy dataset to visualize clustering and classification algorithms."""
 
 datasets = [
     make_moons(noise=0.3, random_state=0),
@@ -61,22 +100,35 @@ datasets = [
     linearly_separable,
 ]
 
+print("dataset: "  , datasets)
+
 figure = plt.figure(figsize=(27, 9))
 i = 1
 # iterate over datasets
 for ds_cnt, ds in enumerate(datasets):
     # preprocess dataset, split into training and test part
     X, y = ds
+
+    #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    """train_test_split
+        sklearn.model_selection.train_test_split(*arrays, test_size=None, train_size=None, random_state=None, shuffle=True, stratify=None)[source]
+        Split arrays or matrices into random train and test subsets.
+
+        Quick utility that wraps input validation, next(ShuffleSplit().split(X, y)), and application to input data into a single call for splitting (and optionally subsampling) data into a one-liner."""
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.4, random_state=42
     )
-
+    
     x_min, x_max = X[:, 0].min() - 0.5, X[:, 0].max() + 0.5
     y_min, y_max = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
 
     # just plot the dataset first
     cm = plt.cm.RdBu
     cm_bright = ListedColormap(["#FF0000", "#0000FF"])
+    """https://www.w3schools.com/python/matplotlib_subplot.asp
+       Display Multiple Plots
+       With the subplot() function you can draw multiple plots in one figure:
+    """
     ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
     if ds_cnt == 0:
         ax.set_title("Input data")
@@ -134,3 +186,5 @@ for ds_cnt, ds in enumerate(datasets):
 
 plt.tight_layout()
 plt.show()
+
+input("Wait for me....")
