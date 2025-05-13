28f71ba6b0d7cc65ff5b08332b1a7697384c6670
ShahzadSarwar10 <e@cognitiveconvergence.com>
2025-05-13 14:28:28 +0500

    added comments - region wise..
    added comments - region wise..

--- Week8/Case8-2-TF-1DConvolutionalNeuralNetworks-CNN-ForTextClassification.py
+++ Week8/Case8-2-TF-1DConvolutionalNeuralNetworks-CNN-ForTextClassification.py
@@ -1,6 +1,7 @@
 """
 https://www.geeksforgeeks.org/text-classification-using-cnn/
 
+
 """
 
 # importing the necessary libraries
@@ -22,6 +23,46 @@ batch_size = 32  # Batch size for training
 epochs = 2  # Number of training epochs
 threshold = 0.5  # Threshold for binary classification
 
+
+"""
+
+https://www.tensorflow.org/api_docs/python/tf/keras/datasets/imdb
+https://keras.io/api/datasets/imdb/
+
+classification dataset
+[source]
+
+load_data function
+keras.datasets.imdb.load_data(
+    path="imdb.npz",
+    num_words=None,
+    skip_top=0,
+    maxlen=None,
+    seed=113,
+    start_char=1,
+    oov_char=2,
+    index_from=3,
+    **kwargs
+)
+
+Loads the IMDB dataset.
+
+This is a dataset of 25,000 movies reviews from IMDB, labeled by sentiment (positive/negative). Reviews have been preprocessed, and each review is encoded as a list
+ of word indexes (integers). For convenience, words are indexed by overall frequency in the dataset, so that for instance the integer "3" encodes the 3rd most frequent 
+ word in the data. This allows for quick filtering operations such as: "only consider the top 10,000 most common words, but eliminate the top 20 most common words".
+
+As a convention, "0" does not stand for a specific word, but instead is used to encode the pad token.
+
+Returns
+
+Tuple of Numpy arrays: (x_train, y_train), (x_test, y_test).
+x_train, x_test: lists of sequences, which are lists of indexes (integers). If the num_words argument was specific, the maximum possible index value is num_words - 1. If the maxlen argument was specified, the largest possible sequence length is maxlen.
+
+y_train, y_test: lists of integer labels (1 or 0).
+
+Note: The 'out of vocabulary' character is only used for words that were present in the training set but are not included because they're not making the num_words cut here. Words that were not seen in the training set but are in the test set have simply been skipped.
+
+"""
 # Loading the IMDB dataset
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=maximum_features)
 
@@ -31,6 +72,58 @@ x_test = pad_sequences(x_test, maxlen=maximum_length)
 
 # Building the model
 model = Sequential()
+
+"""
+https://keras.io/api/layers/core_layers/embedding/
+
+Embedding layer
+[source]
+
+Embedding class
+keras.layers.Embedding(
+    input_dim,
+    output_dim,
+    embeddings_initializer="uniform",
+    embeddings_regularizer=None,
+    embeddings_constraint=None,
+    mask_zero=False,
+    weights=None,
+    lora_rank=None,
+    **kwargs
+)
+Turns nonnegative integers (indexes) into dense vectors of fixed size.
+
+e.g. [[4], [20]] -> [[0.25, 0.1], [0.6, -0.2]]
+
+This layer can only be used on nonnegative integer inputs of a fixed range.
+
+"""
+"""
+Keras Conv1D Layer
+
+The Conv1D layer in Keras is a one-dimensional convolution layer primarily used for temporal data. This layer creates a convolution kernel that is convolved with
+ the layer input over a single spatial (or temporal) dimension to produce a tensor of outputs. If use_bias is set to True, a bias vector is created and added to the 
+ outputs. Finally, if an activation function is specified, it is applied to the outputs as well.
+
+ https://keras.io/api/layers/convolution_layers/convolution1d/
+
+ https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv1D
+
+"""
+
+"""
+GlobalMaxPooling1D layer
+[source]
+
+GlobalMaxPooling1D class
+keras.layers.GlobalMaxPooling1D(data_format=None, keepdims=False, **kwargs)
+Global max pooling operation for temporal data.
+
+https://keras.io/api/layers/pooling_layers/global_max_pooling1d/
+
+https://www.tensorflow.org/api_docs/python/tf/keras/layers/GlobalMaxPool1D
+
+"""
 
 # Adding the embedding layer to convert input sequences to dense vectors
 model.add(Embedding(maximum_features, word_embedding_dims,
@@ -74,3 +167,9 @@ print('Accuracy:', accuracy)
 print('Precision:', precision)
 print('Recall:', recall)
 print('F1-score:', f1)
+
+"""
+See details about accuracy , precision , recall , f1
+https://github.com/ShahzadSarwar10/AI-ML-Explorer/blob/main/Week5/Artificial%20Intelligence%20(Machine%20Learning%20%26%20Deep%20Learning)-Week5%20-%20Machine%20Learning%20-%20Important%20Concepts%20-%20Notes_Rev1.pdf
+
+"""
